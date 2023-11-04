e13e407a9ecb3517ddc7817af02a1eb69ba55033
Luthfi Afriansyah <105446118+luthpai@users.noreply.github.com>
2023-11-04 22:00:42 +0700

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# GH Profile Finder Py
+
+Simple app to search profiles available on GitHub.
+
+## Usage
+
+Install required modules
+```pip install -r requirements.txt```
+
+Run
+```
+  python main.py
+  # or
+  py main.py
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+'''
+ Made by Luthpai ( Luthfi Afriansyah )
+ GitHub : luthpai
+ MIT License 2023©
+ You are able to modify
+ You are able to distribute or sell
+ I'm not liable and responsible of everything
+ Thanks :•D
+'''
+
+import requests
+
+import sys
+sys.tracebacklimit=0
+
+# GitHub API base url
+url = "https://api.github.com/users"
+
+# Ask user to input username
+search_input = input("Input username: ")
+
+# Check if user has inputted username
+if search_input:
+	profile = requests.get(f"{url}/{search_input}")
+
+# Turn the profile result to JSON
+profile = profile.json()
+
+# Check if user us found
+# By checking "login" in the profile result
+if "login" in profile:
+  # If the profile is found then
+  # Declare a new variable with the profile data from the result
+  profile_data = f"""\n
+    username: {profile['login']}
+    name: {profile['name']}
+    link: {profile['blog']}
+    bio: {profile['bio']}
+    location: {profile['location']}
+    followers: {profile['followers']}
+    following: {profile['following']}
+    repositories: {profile['public_repos']}
+  """
+  # Print the data
+  print(profile_data)
+else:
+	# print User not found if "login" is not found
+	print("\n User not found.")
